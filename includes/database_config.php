822eaf876af4ff44910daeca039413ca18024aa4
dianoia-edu <jk@dianoia.de>
2025-03-15 23:41:59 +0100

    ignore

--- includes/database_config.php
+++ includes/database_config.php
@@ -1,5 +1,5 @@
 <?php
-// require_once __DIR__ . '/../config/config.php'; wurde entfernt
+// require_once __DIR__ . '/../config/config.php'; wurde  entfernt
 
 // Datenbankverbindungskonfiguration
 class DatabaseConfig {
