2c547b7121ab309274c3f84dd4e210582123c4b7
dianoia-edu <jk@dianoia.de>
2025-03-17 19:13:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// Aktiviere Fehlerberichterstattung für die Entwicklung
+// Aktiviere Fehlerberichterstattung für dieEntwicklung
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
